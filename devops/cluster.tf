73b407d7e2a04a10a5f7611865993afdc7b97e91
CodingForEntrepreneurs <hello@teamcfe.com>
2023-01-04 14:52:01 -0600

    12 - Terraforming the Kubeconfig File

--- devops/cluster.tf
+++ devops/cluster.tf
@@ -12,7 +12,7 @@ resource "linode_lke_cluster" "terraform-k8s-resource" {
 
 
 resource "local_file" "k8s_config" {
-    content = "Data coming soon"
+    content = "${nonsensitive(base64decode(linode_lke_cluster.terraform-k8s-resource.kubeconfig))}"
     filename = "${local.k8s_config_file}"
     file_permission = "0600"
 }
