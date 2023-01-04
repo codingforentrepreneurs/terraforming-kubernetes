d12fcaa1e13819ca53bde11452965ba4ab105a9f
CodingForEntrepreneurs <hello@teamcfe.com>
2023-01-04 14:19:19 -0600

    9 - Prepare to Terraform A Kubernetes Cluster

--- devops/cluster.tf
+++ devops/cluster.tf
@@ -1,3 +1,16 @@
+resource "linode_lke_cluster" "terraform-k8s-resource" {
+    label       = "terraform-k8s-cluster"
+    k8s_version = "1.24"
+    region      = "us-east"
+    tags        = ["terraform-k8s"]
+
+    pool {
+        type  = "g6-standard-1"
+        count = 3
+    }
+}
+
+
 resource "local_file" "k8s_config" {
     content = "Data coming soon"
     filename = "${local.k8s_config_file}"
